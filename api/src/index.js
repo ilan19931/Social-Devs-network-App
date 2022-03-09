c599d1e754fa2ae55c51dd93b05a4b7ee58ce179
ilan19931 <kinshin1993@gmail.com>
2022-03-10 00:45:49 +0200

    Heroku production deploy.

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const path = require("path");
 
 const dbConnect = require("./helpers/dbConnect");
 const apllyAllRoutes = require("./routes");
@@ -13,15 +13,22 @@ app.use(cors());
 // routes
 apllyAllRoutes(app);
 
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
 
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 try {
-    app.listen(process.env.SERVER_PORT, () => {
-        console.log("Server started on port: " + process.env.SERVER_PORT);
+  app.listen(process.env.SERVER_PORT || 3005, () => {
+    console.log("Server started on port: " + process.env.SERVER_PORT);
 
-        dbConnect();
-    });
+    dbConnect();
+  });
 } catch (err) {
-    console.log(err);
+  console.log(err);
 }
